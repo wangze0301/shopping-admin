2a8ce8a0c9f94512308226a653defb7e3935c716
wangze0301 <454674709@qq.com>
2021-07-29 12:28:08 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
+import ElementUI from 'element-ui'
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/"
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
